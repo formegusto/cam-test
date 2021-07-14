2f58527d187bded7a1855bf72802a9be45799bc9
formegusto <formegusto@gmail.com>
2021-07-14 23:28:07 +0900

    testing okay

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 function load() {
   const video = document.getElementsByTagName("video")[0];
+  const capture = document.getElementById("capture");
 
   navigator.getUserMedia(
     { video: true },
@@ -10,6 +11,20 @@ function load() {
       console.error(error);
     }
   );
+
+  capture?.addEventListener("click", takepicture);
+}
+
+function takepicture() {
+  const canvas = document.getElementsByTagName("canvas")[0];
+  const video = document.getElementsByTagName("video")[0];
+  const photo = document.getElementById("photo");
+  const context = canvas.getContext("2d");
+
+  context?.drawImage(video, 0, 0, 300, 300);
+
+  const data = canvas.toDataURL("image/png");
+  photo?.setAttribute("src", data);
 }
 
 document.body.onload = load;
